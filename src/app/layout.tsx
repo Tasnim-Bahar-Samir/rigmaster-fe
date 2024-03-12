b3c7f675cfd9244ef0f9b5d2c9db56cd65468dc6
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2024-03-13 02:16:09 +0600

    feat: added fb pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import TanStackQueryProvider from '@/components/providers/TanStackQueryProvider'
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/libs/auth/authOptions';
 import NextAuthProvider from '@/components/providers/NextAuthProvider';
+import Script from 'next/script';
 // import { SnackbarProvider } from 'notistack';
 
 const poppins = Poppins({
@@ -39,6 +40,26 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await getServerSession(authOptions);
   return (
     <html lang="en">
+      {/* Meta Pixel Code */}
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '401487229152678');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      {/* End Meta Pixel Code */}
       <body className={poppins.className}>
         <StyledEngineProvider injectFirst>
           <ToasterProvider>
